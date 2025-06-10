29164b9c737f0a37c6b67a3fff1f2449fee40b0a
Martin Curylo <curylo@oltis.cz>
2025-06-10 22:52:35 +0200

    test 3

--- SystemPKSSS/Program.cs
+++ SystemPKSSS/Program.cs
@@ -2,9 +2,17 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// ?? nutné pro naètení appsettings.json manuálnì, pokud nemáš šablonu
+builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+
 // 1. Registrace služeb (DbContext, CORS, apod.)
+
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+Console.WriteLine("?? Connection string:");
+Console.WriteLine(builder.Configuration.GetConnectionString("DefaultConnection"));
 
 // 2. Registrace statických souborù (HTML, CSS, JS)
 builder.Services.AddRouting();
