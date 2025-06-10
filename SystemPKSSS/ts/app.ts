16953c4a6e36e258ec39d8f1d7732c748a928295
Martin Curylo <curylo@oltis.cz>
2025-06-10 22:06:42 +0200

    ts

--- SystemPKSSS/ts/app.ts
+++ SystemPKSSS/ts/app.ts
@@ -1,17 +1,4 @@
-import { createService } from './services.js';
-
-document.getElementById('create-service-btn')?.addEventListener('click', async () => {
-    const nameInput = <HTMLInputElement>document.getElementById('service-name');
-    const descriptionInput = <HTMLInputElement>document.getElementById('service-description');
-
-    try {
-        const result = await createService(nameInput.value, descriptionInput.value);
-        alert(`Služba vytvoøena s ID: ${result.id}`);
-    } catch (error) {
-        alert("Chyba pøi vytváøení služby");
-    }
-});
-import { createService } from './services.js';
+import { createService } from './service.js';
 
 document.getElementById('create-service-btn')?.addEventListener('click', async () => {
     const nameInput = <HTMLInputElement>document.getElementById('service-name');
---
+++ SystemPKSSS/wwwroot/dist/app.js
@@ -0,0 +1,36 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var _a, _b;
+Object.defineProperty(exports, "__esModule", { value: true });
+const service_js_1 = require("./service.js");
+(_a = document.getElementById('create-service-btn')) === null || _a === void 0 ? void 0 : _a.addEventListener('click', () => __awaiter(void 0, void 0, void 0, function* () {
+    const nameInput = document.getElementById('service-name');
+    const descriptionInput = document.getElementById('service-description');
+    try {
+        const result = yield (0, service_js_1.createService)(nameInput.value, descriptionInput.value);
+        alert(`Slu�ba vytvo�ena s ID: ${result.id}`);
+    }
+    catch (error) {
+        alert("Chyba p�i vytv��en� slu�by");
+    }
+}));
+(_b = document.getElementById('create-service-btn')) === null || _b === void 0 ? void 0 : _b.addEventListener('click', () => __awaiter(void 0, void 0, void 0, function* () {
+    const nameInput = document.getElementById('service-name');
+    const descriptionInput = document.getElementById('service-description');
+    try {
+        const result = yield (0, service_js_1.createService)(nameInput.value, descriptionInput.value);
+        alert(`Slu�ba vytvo�ena s ID: ${result.id}`);
+    }
+    catch (error) {
+        alert("Chyba p�i vytv��en� slu�by");
+    }
+}));
+//# sourceMappingURL=app.js.map
